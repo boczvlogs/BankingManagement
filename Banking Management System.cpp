3cf66e2375bd7f5e7e4a151c621712ae62864411
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-12-09 11:36:01 +0800

    Update Banking Management System.cpp

--- Banking Management System.cpp
+++ Banking Management System.cpp
@@ -28,7 +28,7 @@ class BankAccount{
 		void withdraw(double amount){
 			if(balance >= amount){
 				balance = balance - amount; 
-				cout<<"\t\tWithdraw Successfully..."<<endl;
+				cout<<"\t\tWithdraw Successfully THanks..."<<endl;
 			}else{ 
 				cout<<"\t\tInsufficient Balance available...."<<endl;
 			} 
